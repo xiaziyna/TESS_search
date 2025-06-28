e00573fa502c28f40e91a6f07babc4dad982728a
Jamila Taaki <xiaziyna@googlemail.com>
2025-06-27 21:57:31 -0400

    Update noise_cov.py

--- noise_cov.py
+++ noise_cov.py
@@ -9,10 +9,11 @@ from info import *
 for sector in sectors:
     N_cadence = cadence_bounds[sector][1]-cadence_bounds[sector][0]
     filled_lc = np.zeros(N_cadence)
-    cadence = time_data[sector] - cadence_bounds[sector][0]
-    filled_lc[cadence] = threshold_data(detrend_data[sector], level=3)
+    mask = np.zeros(N_cadence, dtype=bool)
+    mask[time_data[sector] - cadence_bounds[sector][0]] = True
+    filled_lc[mask] = threshold_data(detrend_data[sector], level=3)
     std_ = np.std(filled_lc[time_data[sector] - cadence_bounds[sector][0]])
-    filled_lc[~cadence] = np.random.normal(0, std_, N_cadence - len(cadence))
+    filled_lc[~mask] = np.random.normal(0, std_, N_cadence - len(cadence))
     zp_lc = np.zeros((2*N_cadence) - 1)
     zp_lc[:N_cadence] = detrend
     p_noise_smooth = smooth_p(zp_lc, K=3)
