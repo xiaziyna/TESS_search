8eb9f65225b413063a229b6219b1212af886e26c
Jamila Taaki <xiaziyna@googlemail.com>
2025-07-10 16:52:22 -0400

    Update gen_prior.py

--- gen_prior.py
+++ gen_prior.py
@@ -1,1 +1,4 @@
 from util import *
+
+pickle.load(cov_c, open("priors/%s/cov_c%s_%s_%s.p" % (sector, sector, cam, ccd), "wb" ) )
+pickle.dump(cov_c2, open("priors/%s/cov_c_diag%s_%s_%s.p" % (sector, sector, cam, ccd), "wb") )
