8e27408364189e3ca4c3df5393e641799f07a223
Jamila Taaki <xiaziyna@googlemail.com>
2025-07-10 16:02:07 -0400

    Update download_lc.py

--- download_lc.py
+++ download_lc.py
@@ -1,9 +1,9 @@
 import lightkurve as lk
 import pickle
 import numpy as np
-from info import sectors, cadence_bounds
 import os
-
+import matplotlib.pyplot as plt
+from info import sectors, cadence_bounds
 
 tid_list = np.loadtxt('persistant_tids.txt', dtype=int)
 
@@ -12,7 +12,6 @@ for tid in tid_list:
     if os.path.exists(os.path.expanduser(f'~/TESS/data/{tic_id}.p')): 
         continue
     else:
-        raise SystemExit
         lc_data = {}
         processed_lc_data = {}
         time_data = {}
@@ -22,7 +21,8 @@ for tid in tid_list:
         coeff_ls = {}
         detrend_data = {}
         norm_offset = {}
-        quality = {}
+        quality_data = {}
+        pos_xy_corr = {}
         for sector in range(sectors[0], sectors[-1]+1):
             print (sector, tic_id)
             try:
@@ -31,7 +31,7 @@ for tid in tid_list:
             except: 
                 print ('missing sector', tic_id)
                 continue
-            lc_sap = lightcurve.SAP_FLUX 
+            lc_sap = lightcurve.SAP_FLUX
             lc_pdc = lightcurve.PDCSAP_FLUX
             quality = lc_sap.quality 
             lc_data[sector] = lc_sap.flux.to_value()[~quality.astype(bool)]
@@ -39,23 +39,21 @@ for tid in tid_list:
             time_data[sector] = lc_sap.cadenceno[~quality.astype(bool)]
             cam_data[sector] = lightcurve.camera
             ccd_data[sector] = lightcurve.ccd
-            quality[sector] = lightcurve.quality
-            centroid_xy_data[sector] = [lightcurve.centroid_col, lightcurve.centroid_row] #are these the right way around?
-            evecs = pickle.load(open("priors/%s/evec_matrix_%s_%s_%s.p" % (sector, sector, cam_data[sector], ccd_data[sector]), "rb" ) #add correct path!
-            cadence_data = time_data[sector] - cadence_bounds[sector][0]
-            lc_offset = np.nanmedian(lc_sap)
-            lc_sap -= lc_offset
-            lc_norm = np.linalg.norm(lc_sap)
-            lc_sap /= lc_norm
+            qual = np.zeros_like(lc_sap.cadenceno.data)
+            qual[quality.astype(bool)] = 1
+            quality_data[sector] = qual
+            centroid_xy_data[sector] = [lc_sap.mom_centr1.to_value()[~quality.astype(bool)], lc_sap.mom_centr2.to_value()[~quality.astype(bool)]] 
+            pos_xy_corr[sector] = [lc_sap.pos_corr1.to_value()[~quality.astype(bool)], lc_sap.pos_corr2.to_value()[~quality.astype(bool)]]
+            evecs = pickle.load(open("priors/%s/evec_matrix_%s_%s_%s.p" % (sector, sector, cam_data[sector], ccd_data[sector]), "rb" ))
+            cadence_data = time_data[sector] - cadence_bounds[sector][0] -1
+            lc_sap_val = lc_sap.flux.to_value()[~quality.astype(bool)]
+            lc_offset = np.nanmedian(lc_sap_val)
+            lc_sap_val -= lc_offset
+            lc_norm = np.linalg.norm(lc_sap_val)
+            lc_sap_val /= lc_norm
             evecs_mask = evecs[:, cadence_data]
-            coeff = np.dot(evecs_mask, lc.T)
-            coeff_ls[sector] = coeff
+            coeff = np.dot(evecs_mask, lc_sap_val.T)
             norm_offset[sector] = [lc_offset, lc_norm]
-            coeff_tid[sector] = coeff*lc_norm
-            detrend_data[sector] = lc_norm*(lc_sap - np.dot(coeff, evecs)) 
-            # uncomment me on first run, then delete - just to check the output looks fine!
-            # import matplotlib.pyplot as plt
-            # plt.figure
-            # plt.scatter(cadence_data, detrend[sector])
-            # plt.show
-        pickle.dump((lc_data, processed_lc_data, detrend_data, norm_offset, quality, time_data, cam_data, ccd_data, coeff_ls, centroid_xy_data), open('~/TESS/data/%s.p' % (tic_id), 'wb'))
+            coeff_ls[sector] = coeff*lc_norm
+            detrend_data[sector] = lc_norm*(lc_sap_val - np.dot(coeff, evecs_mask)) 
+        pickle.dump((lc_data, processed_lc_data, detrend_data, norm_offset, quality_data, time_data, cam_data, ccd_data, coeff_ls, centroid_xy_data, pos_xy_corr),  open(os.path.expanduser('~/TESS/data/%s.p' % (tic_id)), 'wb')) 
